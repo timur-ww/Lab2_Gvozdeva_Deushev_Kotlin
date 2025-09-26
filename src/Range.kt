0bcdbc42c2e55113b0f18977864875882ae12088
timur <timurkkq@gmail.com>
2025-09-27 01:42:52 +0300

    asdqwe

---
+++ src/Range.kt
@@ -0,0 +1,25 @@
+fun main() {
+    /*val num = 1..100
+    val alf = "a".."z"
+
+
+    println("Введите ваш уровень: ")
+    val lvl = readln().toInt()
+    if (lvl in 1..50 ) {
+        println("Уровень $lvl соответствует требованиям")
+    } else {
+       println("Слишком высокий уровень")
+    }
+
+
+    val abc = "z".."a"
+    println(abc)
+
+    println(5 until 11)
+    val rn = 10..50 step 3
+    for (i in rn){
+        println(i)
+    }*/
+
+
+}
--- src/asdasd.kt
+++ src/asdasd.kt
@@ -1,5 +1,46 @@
 fun main() {
-    /*println("выходите бесы мы станцуем джерси")*/
+    /*println("выходите бесы мы станцуем джерси")
     val num = 10
-    println(num)
+    println(num)*/
+    /*println("Введите первое число ")
+    val num1 = readln().toInt()
+    println("Введите второе число ")
+    val num2 = readln().toInt()
+    if (num1 > num2) {
+        println("Первое число больше ")
+    } else {
+        println("Второе число больше ")
+    }
+
+    val max = if (num1 > num2) num1 else num2
+    println("Мксимальное число $max")*/
+    /*val age = readln().toInt()
+    val status = if (age >= 18) "Совершеннолетний" else "Козявка"
+    println(status)*/
+
+
+    /*val hp: Int = 100
+    println("Введите урон: ")
+    val damage = readln().toInt()
+    println("Щит активен?(да/нет): ")
+    val shield = readln().toBoolean()
+    if (shield == true){
+        hp -= damage / 2
+    } else{
+        hp -= damage
+    }*/
+
+
+    /* println("Введите ваш уровень: ")
+     val lvl = readln().toInt()
+     println("Есть ли випка(true/false): ")
+     val vip = readln().toBoolean()
+     if (lvl >= 30 && vip == true) {
+         println("Доступ разрешен")
+     } else if (lvl >= 50) {
+         println("Доступ разрешен")
+     } else {
+         println("Доступ запрещен")
+     }*/
+
 }
